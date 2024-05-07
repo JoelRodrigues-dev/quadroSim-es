02cd1159b46f7062caba65395eee297cadf5c37f
Joel Rodrigues <joelp.rodrigues25@gmail.com>
2024-05-07 15:00:37 -0300

    final stretch of the project

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const imgsContainer = document.querySelector(".carrossel .container")
+const img = document.querySelectorAll(".carrossel .container img");
+
+
+console.log(imgsContainer)
+
+    console.log(img)
+
+
+let contador = 0;
+
+function slider(){
+    contador++
+   if(contador > img.length -1){
+    contador = 0;
+   }
+
+   imgsContainer.style.transform = `translateX(${-contador * 100}%)`;
+}
+
+
+setInterval(slider, 4000);
